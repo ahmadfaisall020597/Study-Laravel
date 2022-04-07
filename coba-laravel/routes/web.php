a6acc019f7e4323a71bf7c84e8af814b2e1435c5
ahmadfaisal2513 <ahmadfaisal2513@gmail.com>
2022-04-07 12:29:24 +0700

    Struktur Folder, Routes, & Views

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Faisal",
+        "email" => "ahmadfaisal2513@gmail.com",
+        "image" => "laravel.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
