955b9f45fb0ef5f5ea0647f61070a4572b1a4297
ahmadfaisal2513 <ahmadfaisal2513@gmail.com>
2022-04-11 13:12:02 +0700

    Model, Collection & Controller

---
+++ coba-laravel/app/Models/Post.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+
+// hanya untuk bisa di akses untuk kelas ini saja maka menggunakan private static
+class Post
+{
+    // blognya bisa kita ambil dari database / API
+    private static $blog_posts = [
+        [
+            "tittle" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Ahmad Faisal",
+            "body" => "Apa itu Laravel Anda pasti tahu bahasa pemrograman PHP? Laravel adalah satu-satunya framework yang membantu Anda untuk memaksimalkan penggunaan PHP di dalam proses pengembangan website. PHP menjadi bahasa pemrograman yang sangat dinamis, tapi semenjak adanya Laravel, dia menjadi lebih powerful, cepat, aman, dan simpel. Setiap rilis versi terbaru, Laravel  selalu memunculkan teknologi baru di antara framework PHP lainnya."
+        ],
+        [
+            "tittle" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Rara Ayu",
+            "body" => "Laravel diluncurkan sejak tahun 2011 dan mengalami pertumbuhan yang cukup eksponensial. Di tahun 2015, Laravel adalah framework yang paling banyak mendapatkan bintang di Github. Sekarang framework ini menjadi salah satu yang populer di dunia, tidak terkecuali di Indonesia. "
+        ]
+        ];
+
+        public static function all() // kita bisa mendapatkan semua hasil datanya
+        {
+            // untuk properti biasa dapat memakai
+            // return $this->blog_post;
+            // return self untuk properti static
+            // return self::$blog_posts; // untuk biasa
+            return collect(self::$blog_posts); // untuk collection bisa di panggil secara bnyak atau berskala
+        }
+
+        // jika ingin 1 seperti contoh di bawah, kita ambil datanya baru kita looping
+        public static function find($slug) // parameter
+        {
+            $posts = static::all(); // untuk pemanggilan collect parameter all
+            //$posts = self::$blog_posts; // Ambil semua Postnya
+        //     $post = []; // array / postingan yg ketemu
+        // foreach($posts as $p) // kita looping 1 per 1 di presentasikan sebagai $p
+        // {
+        //     if ($p["slug"] === $slug) // jika $slug nya itu sama dengan $slug yg di kirim ke parameter
+        //     {
+        //         $post = $p; // maka masukan postingan tersebut kedapan variabel $post
+        //     }
+        // }
+
+        // return $post; // jika ketemu kembalikan hasilnya
+
+        // source menggantikan dari banyak baris di atas
+        return $posts->firstWhere('slug',$slug); // ambil semua $posts yang bentuknya collection lalu cari yang pertama di temukan yang dimana slugnya sama dengan $slug
+    }
+}
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -35,58 +39,7 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/posts', function () {
-    $posts_posts = [
-        [
-            "tittle" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Ahmad Faisal",
-            "body" => "Apa itu Laravel Anda pasti tahu bahasa pemrograman PHP? Laravel adalah satu-satunya framework yang membantu Anda untuk memaksimalkan penggunaan PHP di dalam proses pengembangan website. PHP menjadi bahasa pemrograman yang sangat dinamis, tapi semenjak adanya Laravel, dia menjadi lebih powerful, cepat, aman, dan simpel. Setiap rilis versi terbaru, Laravel  selalu memunculkan teknologi baru di antara framework PHP lainnya."
-        ],
-        [
-            "tittle" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Rara Ayu",
-            "body" => "Laravel diluncurkan sejak tahun 2011 dan mengalami pertumbuhan yang cukup eksponensial. Di tahun 2015, Laravel adalah framework yang paling banyak mendapatkan bintang di Github. Sekarang framework ini menjadi salah satu yang populer di dunia, tidak terkecuali di Indonesia. "
-        ]
-        ];
-
-    return view('posts', [
-        "tittle" => "Posts",
-        "posts" => $posts_posts
-    ]);
-});
-
-
+Route::get('/posts', [PostController::class, 'index']);
 
 // Halaman Single Posts
-
-Route ::get('posts/{slug}', function($slug){
-    $posts_posts = [
-        [
-            "tittle" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Ahmad Faisal",
-            "body" => "Apa itu Laravel Anda pasti tahu bahasa pemrograman PHP? Laravel adalah satu-satunya framework yang membantu Anda untuk memaksimalkan penggunaan PHP di dalam proses pengembangan website. PHP menjadi bahasa pemrograman yang sangat dinamis, tapi semenjak adanya Laravel, dia menjadi lebih powerful, cepat, aman, dan simpel. Setiap rilis versi terbaru, Laravel  selalu memunculkan teknologi baru di antara framework PHP lainnya."
-        ],
-        [
-            "tittle" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Rara Ayu",
-            "body" => "Laravel diluncurkan sejak tahun 2011 dan mengalami pertumbuhan yang cukup eksponensial. Di tahun 2015, Laravel adalah framework yang paling banyak mendapatkan bintang di Github. Sekarang framework ini menjadi salah satu yang populer di dunia, tidak terkecuali di Indonesia. "
-        ]
-        ];
-
-        $new_post = [];
-        foreach($posts_posts as $post){
-            if ($post["slug"] === $slug){
-                $new_post = $post;
-            }
-        }
-
-
-    return view ('post',[
-        "tittle" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/posts/{slug}', [PostController::class, 'show']);
