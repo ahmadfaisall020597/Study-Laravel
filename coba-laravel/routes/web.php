e86994577204c63cb5efd806203f05a445c1ca55
ahmadfaisal2513 <ahmadfaisal2513@gmail.com>
2022-04-19 13:41:56 +0700

    N+1 Problem (Eager Loading & Lazy Eager Loading)

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -56,13 +56,13 @@ Route::get('/categories', function() {
 Route::get('/categories/{category:slug}', function(Category $category){
     return view('posts',[
         'title' => "Post By Category : $category->name",
-        'posts' => $category->posts,
+        'posts' => $category->posts->load('author','category'), // Penggunaan lazy eager
     ]);
 });
 
 Route::get('/authors/{author:username}', function(User $author) {
     return view('posts',[
         'title' => "Post By Author : $author->name",
-        'posts' => $author->posts,
+        'posts' => $author->posts->load('category','author'), // penggunaan lazy eager load jika penulisan tertukar tidak masalah
     ]);
 });
