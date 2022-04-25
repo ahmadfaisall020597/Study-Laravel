5948f5d3b8bba159e9d93bf7eed3af3135dccea1
ahmadfaisal2513 <ahmadfaisal2513@gmail.com>
2022-04-25 09:26:10 +0700

    Menambahkan Link ke post category

--- coba-laravel/resources/views/posts.blade.php
+++ coba-laravel/resources/views/posts.blade.php
@@ -32,12 +32,13 @@
 
                 <div class="col-md-4 mb-3">
                     <div class="card">
+                        <div class="position-absolute bg-dark px-3 py-2" style="background-color:rgba(0, 0, 0, 0.7)"><a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
                         <img src="http://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
                         <div class="card-body">
                         <h5 class="card-title">{{ $post->title }}</h5>
                         <p>
                             <small class="text-muted">
-                                By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none"> {{ $post->author->name }}</a>{{ $post->created_at->diffForHumans() }}
+                                By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none"> {{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
                             </small>
                         </p>
                         <p class="card-text">{{ $post->excerpt }}</p>
