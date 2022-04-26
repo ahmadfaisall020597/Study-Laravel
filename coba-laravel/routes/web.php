0b9adc57faa939c681c12eff80f9c7856dfdb2db
ahmadfaisal2513 <ahmadfaisal2513@gmail.com>
2022-04-26 09:06:53 +0700

    Revisi Post Page

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -57,18 +57,18 @@ Route::get('/categories', function() {
     ]);
 });
 
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('posts',[
-        'title' => "Post By Category : $category->name",
-        'active' => "categories",
-        'posts' => $category->posts->load('author','category'), // Penggunaan lazy eager
-    ]);
-});
+// Route::get('/categories/{category:slug}', function(Category $category){
+//     return view('posts',[
+//         'title' => "Post By Category : $category->name",
+//         'active' => "categories",
+//         'posts' => $category->posts->load('author','category'), // Penggunaan lazy eager
+//     ]);
+// });
 
-Route::get('/authors/{author:username}', function(User $author) {
-    return view('posts',[
-        'title' => "Post By Author : $author->name",
-        'active' => 'posts',
-        'posts' => $author->posts->load('category','author'), // penggunaan lazy eager load jika penulisan tertukar tidak masalah
-    ]);
-});
+// Route::get('/authors/{author:username}', function(User $author) {
+//     return view('posts',[
+//         'title' => "Post By Author : $author->name",
+//         'active' => 'posts',
+//         'posts' => $author->posts->load('category','author'), // penggunaan lazy eager load jika penulisan tertukar tidak masalah
+//     ]);
+// });
