1835e3e4e05de9a964b6f3363b0f8e58a8609c53
ahmadfaisal2513 <ahmadfaisal2513@gmail.com>
2022-04-28 13:24:57 +0700

    Revisi view login, create route & method login, Otentikasi Login

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -58,10 +58,11 @@ Route::get('/categories', function() {
 });
 
 Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
 
 Route::get('/register', [RegisterController::class, 'index']);
-
 Route::post('/register', [RegisterController::class, 'store']);
+
 
 // Route::get('/categories/{category:slug}', function(Category $category){
 //     return view('posts',[
