5ef44e1d6959c986c68127d112852673d5a899cc
ahmadfaisal2513 <ahmadfaisal2513@gmail.com>
2022-04-28 09:20:29 +0700

    Protect CSRF (Cross Site Request Forgery

--- coba-laravel/resources/views/register/index.blade.php
+++ coba-laravel/resources/views/register/index.blade.php
@@ -7,6 +7,7 @@
         <main class="form-registration">
             <h1 class="h3 mb-3 fw-normal text-center">FORM REGISTRATION</h1>
             <form action="/register" method="post">
+                @csrf
               {{-- <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> --}}
 
               <div class="form-floating">
